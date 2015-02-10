80a7741a5f3f9ccbf307da54921bfe81935557ce
Eric Fowler <ericfowler303@gmail.com>
2015-02-10 16:17:05 -0700

    Initial Commit

---
+++ CSV-ListeningHabits/CSV-ListeningHabits/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSV_ListeningHabits
+{
+    class Program
+    {
+        // Global List
+        public static List<Play> musicDataList = new List<Play>();
+        static void Main(string[] args)
+        {
+            // initalize dataset into list
+            InitList();
+            // keep console open
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// A function to initalize the List from the csv file
+        /// needed for testing
+        /// </summary>
+        public static void InitList()
+        {
+            // load data
+            using (StreamReader reader = new StreamReader("scrobbledata.csv"))
+            {
+                // Get and don't use the first line
+                string firstline = reader.ReadLine();
+                // Loop through the rest of the lines
+                while (!reader.EndOfStream)
+                {
+                    musicDataList.Add(new Play(reader.ReadLine()));
+                }
+            }
+        }
+        /// <summary>
+        /// A function that will return the total ammount of plays in the dataset
+        /// </summary>
+        /// <returns>total number of plays</returns>
+        public static int TotalPlays()
+        {
+            return 0;
+        }
+        /// <summary>
+        /// A function that returns the number of plays ever by an artist
+        /// </summary>
+        /// <param name="artistName">artist name</param>
+        /// <returns>total number of plays</returns>
+        public static int TotalPlaysByArtistName(string artistName)
+        {
+            return -1;
+        }
+        /// <summary>
+        /// A function that returns the number of plays by a specific artist in a specific year
+        /// </summary>
+        /// <param name="artistName">artist name</param>
+        /// <param name="year">one year</param>
+        /// <returns>total plays in year</returns>
+        public static int TotalPlaysByArtistNameInYear(string artistName, string year)
+        {
+            return 0;
+        }
+        /// <summary>
+        /// A function that returns the number of unique artists in the entire dataset
+        /// </summary>
+        /// <returns>number of unique artists</returns>
+        public static int CountUniqueArtists()
+        {
+            return 0;
+        }
+        /// <summary>
+        /// A function that returns the number of unique artists in a given year
+        /// </summary>
+        /// <param name="year">year to check</param>
+        /// <returns>unique artists in year</returns>
+        public static int CountUniqueArtists(string year)
+        {
+            return 0;
+        }
+        /// <summary>
+        /// A function that returns a List of unique strings which contains
+        /// the Title of each track by a specific artists
+        /// </summary>
+        /// <param name="artistName">artist</param>
+        /// <returns>list of song titles</returns>
+        public static List<string> TrackListByArtist(string artistName)
+        {
+            return new List<string>();
+        }
+        /// <summary>
+        /// A function that returns the first time an artist was ever played
+        /// </summary>
+        /// <param name="artistName">artist name</param>
+        /// <returns>DateTime of first play</returns>
+        public static DateTime FirstPlayByArtist(string artistName)
+        {
+            return new DateTime(1);
+        }
+        /// <summary>
+        ///                     ***BONUS***
+        /// A function that will determine the most played artist in a specified year
+        /// </summary>
+        /// <param name="year">year to check</param>
+        /// <returns>most popular artist in year</returns>
+        public static string MostPopularArtistByYear(string year)
+        {
+            return string.Empty;
+        }
+    }
+
+    public class Play
+    {
+        // Properties
+        public DateTime Time { get; set; }
+        public string Artist { get; set; }
+        public string Title { get; set; }
+        public string Album { get; set; }
+        public Play(string lineInput)
+        {
+            // Split using the tab character due to the tab delimited data format
+            string[] playData = lineInput.Split('\t');
+            
+            // Get the time in milliseconds and convert to C# DateTime
+            DateTime posixTime = DateTime.SpecifyKind(new DateTime(1970, 1, 1), DateTimeKind.Utc);
+            this.Time = posixTime.AddMilliseconds(long.Parse(playData[0]));
+
+            // need to populate the rest of the properties
+
+        }
+    }
+}
---
+++ CSV-ListeningHabits/CSV-ListeningHabits/Test.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CSV_ListeningHabits
+{
+    [TestFixture]
+    class Test
+    {
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            Program.InitList();
+        }
+        [Test, Timeout(500)]
+        public void testTotalPlays()
+        {
+            Assert.That(Program.TotalPlays() == 20475);
+        }
+        [Test, Timeout(500)]
+        public void testTotalPlaysByArtistName1()
+        {
+            Assert.That(Program.TotalPlaysByArtistName("All Time Low") == 525);
+        }
+        [Test, Timeout(500)]
+        public void testTotalPlaysByArtistName2()
+        {
+            Assert.That(Program.TotalPlaysByArtistName("nickelback") == 1);
+        }
+        [Test, Timeout(500)]
+        public void testTotalPlaysByArtistName3()
+        {
+            Assert.That(Program.TotalPlaysByArtistName("britney spears") == 0);
+        }
+        [Test, Timeout(500)]
+        public void testTotalPlaysByArtistNameInYear()
+        {
+            Assert.That(Program.TotalPlaysByArtistNameInYear("Skrillex","2014") == 140);
+        }
+        [Test, Timeout(500)]
+        public void testCountUniqueArtistsAll()
+        {
+            Assert.That(Program.CountUniqueArtists() == 1866);
+        }
+        [Test, Timeout(500)]
+        public void testCountUniqueArtists2014()
+        {
+            Assert.That(Program.CountUniqueArtists("2014") == 390);
+        }
+        [Test, Timeout(500)]
+        public void testCountUniqueArtists2007()
+        {
+            Assert.That(Program.CountUniqueArtists("2007") == 502);
+        }
+        [Test, Timeout(500)]
+        public void testTrackListByArtist()
+        {
+            List<string> trackList = new List<string>() {"Applied Science", "Eons", "You Would'nt Believe", "Beautiful Disaster", "All Mixed Up","Come Original"};
+            Assert.AreEqual(trackList, Program.TrackListByArtist("311"));
+        }
+        [Test, Timeout(500)]
+        public void testFirstPlayByArtist()
+        {
+            DateTime first = new DateTime(2007,7,11,19,5,7);
+            Assert.AreEqual(first, Program.FirstPlayByArtist("cartel"));
+        }
+        [Test, Timeout(500)]
+        public void testMostPopularArtistByYear2008()
+        {
+            StringAssert.AreEqualIgnoringCase("Say Anything",Program.MostPopularArtistByYear("2008"));
+        }
+        [Test, Timeout(500)]
+        public void testMostPopularArtistByYear2014()
+        {
+            StringAssert.AreEqualIgnoringCase("ODESZA", Program.MostPopularArtistByYear("2014"));
+        }
+    }
+}
